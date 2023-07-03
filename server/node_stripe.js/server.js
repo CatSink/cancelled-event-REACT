02c828aa413c89944ddbc3d5ef70323b393b0330
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-07-02 20:36:34 -0400

    added api route

--- server/node_stripe.js/server.js
+++ server/node_stripe.js/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+const striperoutes = require('./routes/stripe-routes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
+
+app.use('/api/stripe', striperoutes);
 
 app.listen(5000, () => {
     console.log("Listening on port 5000", 5000);
