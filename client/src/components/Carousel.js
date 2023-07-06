74f1d5ff6f6b4c231b2c5049372e03f64c48d672
Janiece-Lewis <jlewilewi@gmail.com>
2023-07-06 16:35:20 -0400

    added carousel function

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { Carousel } from 'flowbite-react';
+
+
+const MyCarousel = () => {
+
+    return (
+       
+    <Carousel>
+      <img
+        alt="..."
+        src="../../images/sports.png"
+      />
+      <img
+        alt="..."
+        src="../../images/relax.png"
+      />
+      <img
+        alt="..."
+        src="../../images/hangout.png"
+      />
+      <img
+        alt="..."
+        // src="../../images/tea.png"
+      />
+      <img
+        alt="..."
+        src="../../images/flower.png"
+      />
+    </Carousel>
+    
+  )
+}
+
+
+
+
+
+
+
+export default MyCarousel;
