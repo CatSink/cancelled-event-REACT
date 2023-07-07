e090ce0f1e67fb9bbc4e703c202fefc133bd11c3
smilillo <sofiemilillo@gmail.com>
2023-07-06 20:21:16 -0400

    commit before pull

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,7 +1,4 @@
 const { Schema, model } = require('mongoose');
-// const dateFormat = require('../utils/dateFormat');
-
-// const EVENT_MAX_DATE = "2024-01-01";
 
 const eventSchema = new Schema({
   eventTitle: {
@@ -26,29 +23,10 @@ const eventSchema = new Schema({
   invitees: {
     type: String,
     required: true,
+  },
+  {
     
   }
-  // startAt: {
-  //   type: Date,
-  //   required: true,
-  //   validate: {
-  //     validator: function (value) {
-  //       const greaterThanToday = value > new Date() - 1000 * 60 * 60 * 26;
-  //       const limitTo2023 = value < new Date(EVENT_MAX_DATE);
-  //       return greaterThanToday && limitTo2023;
-  //     },
-  //   },
-  // },
-  // endAt: {
-  //   type: Date,
-  //   required: true,
-  //   validate: {
-  //     validator: function (value) {
-  //       return value > this.startAt;
-  //     },
-  //     message: "should be greater than 'startAt' field",
-  //   },
-  // },
 });
 
 const Event = model('Event', eventSchema);
