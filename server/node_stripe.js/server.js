6138d0b15a26661b78c3d95ed2ee993bbe7de634
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-07-08 11:01:28 -0400

    per stripe documentation

--- server/node_stripe.js/server.js
+++ server/node_stripe.js/server.js
@@ -1,21 +1,3 @@
-/*const express = require('express');
-const cors = require('cors');
-
-const striperoutes = require('./routes/stripe-routes');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-
-app.use('/api/stripe', striperoutes);
-
-app.listen(5000, () => {
-    console.log("Listening on port 5000");
-});
-*/
-
 const stripe = require('stripe')('sk_test_51NOVuVKkLuEJzlLFbdMnfjIcAe98oy7aR82NzUVW9YJhOUzPAzjJHRQW6DAk9HjOP6ipvFWR8fqE2MgNwBukA89300OaMzMtuQ');
 const express = require('express');
 const app = express();
