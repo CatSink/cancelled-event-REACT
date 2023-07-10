4a6a3d998dafa997590713330545d05e9c656a98
Janiece-Lewis <jlewilewi@gmail.com>
2023-07-10 17:56:36 -0400

    updated image

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -4,11 +4,12 @@ import { useLocation } from 'react-router-dom';
 function NotFound() {
   let location = useLocation();
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
+    <div className="card bg-white card-rounded w-50 p-11">
+      <div className="card-header bg-dark text-center flex">
         <h1>
           No match for <code>{location.pathname}</code>
         </h1>
+        <img src='./images/notfound.svg'/>
       </div>
     </div>
   );
