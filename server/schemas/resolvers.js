1be8d35b78cc280f1336c6b0b7b84387a9fda103
CatSink <raindropwebweaver@live.com>
2023-07-12 21:17:03 -0400

    updated gql persistent data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,13 +33,14 @@ const resolvers = {
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
+      console.log(password);
 
       if (!user) {
         throw new AuthenticationError('No user found with this email address');
       }
 
       const correctPw = await user.isCorrectPassword(password);
-
+      console.log(correctPw);
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
